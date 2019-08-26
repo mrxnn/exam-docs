27c4917b92f25f0f8b5ad851405aa908bbc02e8a
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2019-08-26 18:40:55 +0530

    Added the startup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
