a8f83e18e41d63d814e3887aba1673556dfe360f
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2019-10-08 18:36:38 +0530

    Added production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyALBBLEnj4pQ7ZmukzlEU5xg8wYF-fbIMw",
+    authDomain: "exam-docs-b28b6.firebaseapp.com",
+    databaseURL: "https://exam-docs-b28b6.firebaseio.com",
+    projectId: "exam-docs-b28b6",
+    storageBucket: "",
+    messagingSenderId: "783208429423",
+    appId: "1:783208429423:web:0975ab976bb3d6a4"
+  },
+  algolia: {
+    apiKey: 'c45b428f5d5fea0d43642a540c85d3f2',
+    appId: '28IX9IUVZI'
+  }
 };
