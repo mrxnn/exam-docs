6fba4a2e8c1c1e79c3097d5fb104263a2289b6ea
Mayura Ramanayaka <mayura.ramanayake@gmail.com>
2019-10-28 04:26:11 +0530

    Some style changes to admin panel

--- src/app/admin/admin-home/admin-home.component.ts
+++ src/app/admin/admin-home/admin-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { environment } from '../../../environments/environment';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-admin-home',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-home.component.scss']
 })
 export class AdminHomeComponent implements OnInit {
+  tinymceInit;
+  apiKey = environment.tinyMCE.apiKey;
 
-  constructor() { }
+  constructor(private location: Location, private auth: AuthService) {
+    this.tinymceInit = {
+      plugins : [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen emoticons paste",
+        "insertdatetime media nonbreaking save table directionality template textpattern"
+      ],
+      toolbar : 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat'
+    }
+  }
 
   ngOnInit() {
   }
 
+  onCloseButtonClicked() {
+    this.location.back();
+  }
+
+  onLogoutButtonClicked() {
+    this.auth.signOut('/');
+  }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
-import { CreateContentComponent } from './create-content/create-content.component';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 @NgModule({
-  declarations: [CreateContentComponent, AdminHomeComponent],
+  declarations: [AdminHomeComponent],
   imports: [
     CommonModule,
     EditorModule,
